837364a0e4b25f24040134ec4fe0fa8987e92d3f
Axel Sotelo <albertosotelo26@hotmail.com>
2022-04-07 15:47:38 -0500

    dispotify

---
+++ dispotify/precache-manifest.d4ad5978cc62ab7682c1540b3f4e458f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e794c643e8293452359e60fc1315a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d37f91f4b6ad23b2e9",
+    "url": "/static/css/main.14ca2fac.chunk.css"
+  },
+  {
+    "revision": "d8aebf580ed797a68a6e",
+    "url": "/static/js/2.0e03dad7.chunk.js"
+  },
+  {
+    "revision": "ba3b4500f00880cf9656c39900077397",
+    "url": "/static/js/2.0e03dad7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79d37f91f4b6ad23b2e9",
+    "url": "/static/js/main.b5e23956.chunk.js"
+  },
+  {
+    "revision": "14bfc2943b0c3baab7ae",
+    "url": "/static/js/runtime-main.7cf23acd.js"
+  },
+  {
+    "revision": "c2706eafd3995f07dc1ca94d89572939",
+    "url": "/static/media/Dispotify.c2706eaf.png"
+  }
+]);
